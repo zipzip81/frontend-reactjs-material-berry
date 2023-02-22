1f5a6f9dbe826351adfa2977627c3f6b7b1112bf
zipzip81 <tankhimsuan@commercedc.com.my>
2023-02-22 10:26:25 +0000

    aaa

--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -22,16 +22,10 @@ const SamplePage = () => {
 
     return (
         <MainCard title="Sample Card">
-            <Typography variant="body2">
-                Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif
-                ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-                reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa
-                qui officiate descent molls anim id est labours.
-      </Typography>
             <TextField label="Name" value={name} onChange={handleNameChange} />
             <Button variant="contained" onClick={handleButtonClick}>
                 Submit
-      </Button>
+            </Button>
             <Typography variant="body1" sx={{ marginTop: '1rem' }}>
                 Response: {message}
             </Typography>
